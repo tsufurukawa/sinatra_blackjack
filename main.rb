22aa7799d3702efb480e9754be0bf98f1b371377
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-04-28 16:12:53 -0700

    Minor refactoring

--- main.rb
+++ main.rb
@@ -12,10 +12,10 @@ helpers do
     ace_count = 0 
     total = 0
     cards.each do |card|
-      if card[0] == 'Ace'
+      if card[0] == "Ace"
         ace_count += 1
         total += 11
-      elsif card[0] == 'Jack' || card[0] == 'Queen' || card[0] == 'King'
+      elsif card[0] == "Jack"|| card[0] == "Queen" || card[0] == "King"
         total += 10
       else
         total += card[0].to_i
@@ -40,15 +40,20 @@ helpers do
 
   def decide_winner(player_cards, dealer_cards)
     @play_again_btn = true
+    @show_hit_or_stay_buttons = false
 
-    if evaluate_total(player_cards) > 21
+    if busted?(player_cards)
       session[:win_amount] = -session[:bet_amount]
       session[:money] += session[:win_amount]          
       "BUST!! #{session[:username].upcase} LOSES $#{session[:bet_amount]}..."
-    elsif evaluate_total(dealer_cards) > 21
+    elsif hit21?(player_cards) && player_cards.count == 2 && busted?(dealer_cards)
+      session[:win_amount] = 1.5 * session[:bet_amount]
+      session[:money] += session[:win_amount]
+      "BLACKJACK!!! #{session[:username].upcase} WINS $#{session[:win_amount]}!!!"
+    elsif busted?(dealer_cards)
       session[:win_amount] = session[:bet_amount]
       session[:money] += session[:win_amount]
-      "BUST!! #{session[:username].upcase} WINS $#{session[:bet_amount]}!!!"
+      "DEALER BUSTS!! #{session[:username].upcase} WINS $#{session[:bet_amount]}!!!"
     elsif evaluate_total(player_cards) > evaluate_total(dealer_cards)
       if hit21?(player_cards) && player_cards.count == 2
         session[:win_amount] = 1.5 * session[:bet_amount]
@@ -77,7 +82,7 @@ helpers do
   end
 
   def hit21?(card)
-    evaluate_total(card) == 21
+    evaluate_total(card) == BLACKJACK
   end
 
   def show_result?(player, dealer)
@@ -85,11 +90,15 @@ helpers do
   end
 end
 
+before do 
+  @show_hit_or_stay_buttons = true
+end
+
 get '/' do 
   erb :set_name
 end
 
-post '/' do
+post '/set_name' do
   if params[:username].lstrip.rstrip.empty?
     @error = "Please enter a valid name"
     erb :set_name
@@ -121,7 +130,7 @@ end
 
 get '/game' do 
   # initialize deck
-  values = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
+  values = %w( 2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
   suits = %w(Spades Clubs Diamonds Hearts)
   session[:win_amount] = 0
   session[:stay] = false
@@ -134,13 +143,22 @@ get '/game' do
   session[:player_hand] << session[:deck].pop
   session[:dealer_hand] << session[:deck].pop
 
-  @dealer_turn_btn = true if hit21?(session[:player_hand])
+  if hit21?(session[:player_hand])
+    @dealer_turn_btn = true
+    @show_hit_or_stay_buttons = false
+  end
+
   erb :game
 end
 
 post '/game/player_hit' do
   session[:player_hand] << session[:deck].pop
-  @dealer_turn_btn = true if hit21?(session[:player_hand])
+
+  if hit21?(session[:player_hand])
+    @dealer_turn_btn = true
+    @show_hit_or_stay_buttons = false
+  end
+
   erb :game
 end
 
@@ -151,6 +169,7 @@ post '/game/player_stay' do
 end
 
 get '/game/dealer' do 
+  @show_hit_or_stay_buttons = false
   erb :game
 end
 
